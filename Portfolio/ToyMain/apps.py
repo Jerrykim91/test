d3e5fd5da9de376d96c89f392c90a70db055f0f9
kim hyowon <sun4131@gmail.com>
2020-08-09 17:05:03 +0900

    err

--- Portfolio/ToyMain/apps.py
+++ Portfolio/ToyMain/apps.py
@@ -1,5 +1,5 @@
-from django.apps import AppConfig
+# from django.apps import AppConfig
 
 
-class ToymainConfig(AppConfig):
-    name = 'ToyMain'
+# class ToymainConfig(AppConfig):
+#     name = 'ToyMain'
